13cf3e7b3b7d7464fc7fd42364571c999ea0493c
Harrison Ingram-Bate <harrison.ingrambate@gmail.com>
2024-07-20 03:11:07 -0700

    Added Line and Filled Polygon Functionality
    Small Change
    Press X for Lines
    Press F for Filled

--- src/main.cpp
+++ src/main.cpp
@@ -131,4 +131,8 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 void processInput(GLFWwindow *window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+    if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS)
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 }
